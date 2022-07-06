9b1b3aeceed0aed641395b49507591d11ec0c1fa
Vedant Jhawar <89154560+vjhawar12@users.noreply.github.com>
2022-07-06 07:30:59 -0400

    Update Constants.java

---
+++ everybot2022/src/main/java/frc/robot/Constants.java
@@ -0,0 +1,14 @@
+package frc.robot; 
+
+public interface Constants {
+    public int FRONT_LEFT = 0; 
+    public int FRONT_RIGHT = 1; 
+    public int REAR_LEFT = 2; 
+    public int REAR_RIGHT = 3; 
+    public int ARM = 5; 
+    public int INTAKE = 6; 
+    public double armTravel = 0.5; 
+    public double armHoldUp = 0.08;
+    public double armTimeUp = 0.5;  
+    public double armTimeDown = 0.35; 
+}
